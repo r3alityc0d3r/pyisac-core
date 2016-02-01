016a9c0a55771244142e56da3ab7f1462cf72a5f
r3alityc0d3r <bill.t.ward@twc.com>
2016-02-01 14:19:00 -0600

    testing something

--- features/steps/command.py
+++ features/steps/command.py
@@ -4,12 +4,11 @@ from hamcrest import *
 
 @given(u'the user runs pyisac from the command line')
 def step_impl(context):
-    context.Main = Main()
+    context.main = Main()
 
 @when(u'no arguments are given')
 def step_impl(context):
-    pass
-    #context.Main.run("")
+    context.main.run("")
 
 @then(u'a banner should be shown')
 def step_impl(context):
