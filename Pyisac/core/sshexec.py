75af2217c51f40869d9fb0198cb25492fa8ef357
r3alityc0d3r <bill.t.ward@twc.com>
2016-02-01 16:06:04 -0600

    adding pydoc html

--- Pyisac/core/sshexec.py
+++ Pyisac/core/sshexec.py
@@ -1,14 +1,31 @@
+"""
+Execute a command over SSH
+"""
 import subprocess
 import sys
 
 class sshexec(object):
-
+    """
+    Executes a command over SSH
+    """
     def __init__(self,host,username,command):
+        """
+        initializes the sshexec command
+
+        :param host: host to run the command on
+        :param username: user for SSH connection
+        :param command: command to run 
+        """
         self.host = host
         self.command = command
         self.username = username
 
     def run(self):
+        """
+        Runs the SSH command and gets output
+
+        :return: result of running the command
+        """
         host_address = "{0}@{1}".format(self.username,self.host)
         ssh = subprocess.Popen(["ssh", "%s" % host_address, self.command],
                 shell = False,
--- examples/report.py
+++ examples/report.py
@@ -2,9 +2,11 @@
 
 def get_kernel_version(host):
     command = "uname -r"
-    return my_infrastructure.exec_command(host, "vagrant", command)
+    my_core = Core()
+    result = my_core.exec_command(host, "vagrant", command)
+    return result
 
 print "system,kernel version"
-for node in nodes:
+for node in self.nodes:
     kernel_version = get_kernel_version(node.servername)
     print "{0},{1}".format(node.servername,kernel_version)
